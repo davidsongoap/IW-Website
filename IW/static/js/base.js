16346d8357de6f4cf687a40e2dda4a2a6dff008e
Davidson Gonçalves <davidsongoap@gmail.com>
2021-04-24 15:24:58 +0100

    created base.js

---
+++ IW/static/js/base.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.ui.dropdown').dropdown();
+
+    $('.special.cards .image').dimmer({
+        on: 'hover',
+        duration: {
+            show: 400,
+            hide: 400
+        }
+    });
+});
