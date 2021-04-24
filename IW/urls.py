fe77e39330a946ea8ecc6d36a5d292837d145985
Davidson Gonçalves <davidsongoap@gmail.com>
2021-04-24 15:26:47 +0100

    added tournament redirect for the search bar

--- IW/urls.py
+++ IW/urls.py
@@ -5,10 +5,11 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", views.home, name="home"),  # home page
-    # path("confirmEmail/", views.confirm, name="confirm"),  # pagina de confirmaçao de email
-    # path("createTournament/", views.create, name="create"),  # pagina para criar torneios
+    path("confirmEmail/", views.confirm, name="confirm"),  # pagina de confirmaçao de email
+    path("createTournament/", views.create, name="create"),  # pagina para criar torneios
     path("tournaments/", views.tournaments, name="tournaments"),  # pagina com todos os torneios
     path("tournament/<str:id>/", views.tournament, name="tournament"),  # pagina de cada torneio (visto pelo id)
+    path("redirectTournament", views.tournament_redirect, name="tournament"),  # pagina de cada torneio (visto pelo id)
     path("login/", auth_views.LoginView.as_view(template_name='registration/login.html', authentication_form=Login),
          name='login'),
 ]
