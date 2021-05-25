2cebfd331cb08e38eed69997d5e3c0e3e3517740
Davidson <davisongoap@gmail.com>
2021-05-25 16:49:12 +0100

    fixed media url path

--- IselWinner/urls.py
+++ IselWinner/urls.py
@@ -13,8 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from IselWinner import settings
 from register import views as v
 
 urlpatterns = [
@@ -27,3 +30,6 @@ urlpatterns = [
     path('', include("django.contrib.auth.urls")),
 
 ]
+if settings.DEBUG:
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
