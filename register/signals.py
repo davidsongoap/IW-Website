34650cf6779c5453f037a563d7209a91dd0932b4
Davidson <davisongoap@gmail.com>
2021-05-25 17:00:05 +0100

    added signal receiver on the participation table

--- register/signals.py
+++ register/signals.py
@@ -4,21 +4,24 @@ import string
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from IW.models import ConfirmationCode, ProfileImage
+from IW.models import ConfirmationCode, ProfileImage, Participation, Tournament
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
+from IW.views import is_tournament_full
 
 
 @receiver(post_save, sender=User)
 def login_logger(sender, instance, created, **kwargs):
     if created:
         cc = ConfirmationCode(user=instance, code=generate_code())
-        # print("New Code generated", cc)
+        if instance.is_superuser:
+            cc.validated = True
         pi = ProfileImage(user=instance)
 
         pi.save()
         cc.save()
 
+        # TODO email with html
         send_mail(
             'ISEL Winner Confirmation Code',
             'Your confirmation code is: ' + str(cc.code),
@@ -26,6 +29,15 @@ def login_logger(sender, instance, created, **kwargs):
             recipient_list=[instance.email],
             fail_silently=False
         )
+
+
+@receiver(post_save, sender=Participation)
+def add_participation(sender, instance, created, **kwargs):
+    if created:
+        t = instance.tournament_id
+        if is_tournament_full(t):
+            t.state = 'CL'
+            t.save()
 
 
 def generate_code(size=15):
