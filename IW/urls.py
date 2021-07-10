7deea1777a7f91ec0442dd2e383f5c19cdd36d87
Davidson <davisongoap@gmail.com>
2021-07-10 13:26:40 +0100

    changed argument name

--- IW/urls.py
+++ IW/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("confirmEmail/", views.confirm, name="confirm"),  # pagina de confirmaçao de email
     path("createTournament/", views.create, name="create"),  # pagina para criar torneios
     path("tournaments/", views.tournaments, name="tournaments"),  # pagina com todos os torneios
-    path("tournament/<str:id>/", views.tournament, name="tournament"),  # pagina de cada torneio (visto pelo id)
+    path("tournament/<str:code>/", views.tournament, name="tournament"),  # pagina de cada torneio (visto pelo id)
     path("redirectTournament", views.tournament_redirect, name="tournament"),  # pagina de cada torneio (visto pelo id)
     path("login/", auth_views.LoginView.as_view(template_name='registration/login.html', authentication_form=Login),
          name='login'),
